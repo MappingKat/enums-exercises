5e07f0fba606fa37c2b7fb115a810f3ec19e2a65
Katrina Engelsted <katrina@engelsted.co>
2013-09-17 13:37:57 -0600

    made it run

--- basic_enums_test.rb
+++ basic_enums_test.rb
@@ -1,21 +1,23 @@
 gem 'minitest'
-require 'minitest'
+require 'minitest/autorun'
 require 'minitest/pride'
 
 class BasicEnumsTest < Minitest::Test
   def test_many_times
     s = ''
-    5.times do
-      # write code here
+    5.times do   
+      s << 'x'
+      puts s  
     end
     assert_equal 'xxxxx', s
   end
 
   def test_simple_sum
-    skip
-    sum = 0
-    numbers = [1, 2, 3, 4, 5]
-    # write code here
+    # sum = 0
+    numbers = [1, 2, 3, 4, 5] #array
+    numbers.each do |number|  #message to send to array to loop through array =1,=2,=3,etc
+      sum += number
+    end
     assert_equal 15, sum
   end
 end
